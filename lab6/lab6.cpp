080d4af3a6fcb9cd0bc70df5264052e906aa3669
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-04-21 03:21:18 +0300

    lab

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,10 +1,64 @@
 #include "tree.h"
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct info 
+{
+    int key;
+    string data;
+};
+
+
 int main()
 {
     Tree t;
+    int size;
+    cout << "Enter amount of elements: ";
+    cin >> size;
+    info arr[size];
+    cout << "Enter info:\n";
 
-    t.Add("ede", 10);
+    for (int i = 0; i < size; i++)   //написать проверку ввода
+    {
+        cout << "Enter key: ";
+        cin >> arr[i].key;
+        cout << "Enter data: ";
+        cin >> arr[i].data;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        t.Add(arr[i].data, arr[i].key);
+    }
+
+    t.ViewInOrder();
+    cout << endl;
+    t.ViewPreOrder();
+    cout << endl;
+    t.ViewPostOrder();
+    cout << endl;
+    
+    cout << "Amount of leaves: " << t.countLeaves() << endl;
+
+    cout << "Enter key to search for the info: ";
+    int key;
+    cin >> key;
+    cout << t.SearchByKey(key) << endl;
+    cout << "Enter key to delete node: ";
+    cin >> key;
+    t.DeleteByKey(key);
+    //cout << t.SearchByKey(key) << endl;
+
+    return 0;
+}
+
+
+
+
+
+/* t.Add("ede", 10);
     t.Add("chek", 25);
     t.Add("wer", 20);
     t.Add("wf4eg", 6);
@@ -17,4 +71,69 @@ int main()
     t.ViewPreOrder();
     std::cout << std::endl;
     t.ViewPostOrder();
-}
+    Tree tr;
+    cout << t.countLeaves() << endl;
+    string s = t.Search(10);
+    cout << s << endl;
+    t.DeleteByKey(10);
+    s = t.Search(10);
+    cout << s << endl; */
+
+
+
+
+
+
+
+/* 
+    void QuickSort(info arr[], int size)
+{
+    int stack[size];          // хранит границы массивов
+
+    int start = 0;
+    int end = size - 1;
+
+    int top = -1;
+
+    stack[++top] = start;
+    stack[++top] = end;
+
+
+    while (top >= 0)
+    {  
+        end = stack[top--];
+        start = stack[top--];
+
+        int i = start;
+        int j = end;
+        int oporn = arr[(start + end) / 2].key;
+        
+        while (i <= j)
+        {
+
+            while (arr[i].key < oporn) i++;                    
+            while (arr[j].key > oporn) j--;
+
+            if (i <= j)
+            {
+                info temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;                                                                                                                  
+            }
+        }
+
+        if (i < end)
+        {
+            stack[++top] = i;
+            stack[++top] = end;
+        }
+
+        if (j > start)
+        {
+            stack[++top] = start;
+            stack[++top] = j;
+        }
+    }
+} */
--- lab6/tree.h
+++ lab6/tree.h
@@ -52,6 +52,14 @@ class Tree
                         std::cout << t->key << ": " << t->data << std::endl;
                     };
                 }
+                int countLeaves(Node * t)
+                {
+                    if (!t)
+                        return 0;
+                    if (!t->left && !t->right)
+                        return 1;
+                    return countLeaves(t->left) + countLeaves(t->right);        
+                }
         };
         Node *root;
 
@@ -65,12 +73,13 @@ class Tree
         Tree(Node *node) : root(node) {}
         ~Tree();
         void Add(string str = "empty", int num = 0);
-        void DeleteNode(int num);
-        void Search();
+        string SearchByKey(int key);
         void ViewInOrder() { root->ViewInOrder(root); }
         void ViewPreOrder() { root->ViewPreOrder(root); }
         void ViewPostOrder() { root->ViewPostOrder(root); }
-        void createBalance();
+        //void Balance();
+        void DeleteByKey(int key);
+        int countLeaves();
 };
 
 
@@ -83,7 +92,7 @@ Tree::~Tree()
 
 void Tree::Add(string str, int num)
 {
-    Node *node = new Node(str, num);
+    Node * node = new Node(str, num);
     if (root == nullptr)
     {
         root = node;
@@ -109,6 +118,106 @@ void Tree::Add(string str, int num)
         prev->left = t;
     else
         prev->right = t;
+}
+
+
+string Tree::SearchByKey(int key) 
+{
+    if (key < 1)
+        return "incorrect key";
+    if (!root)
+        return "tree is empty";    
+    Node * t = root;
+    while (t)
+    {
+        if (key < t->key)
+        {
+            t = t->left;
+        }
+        else if (key > t->key)
+        {
+            t = t->right;
+        }
+        else
+        {
+            return t->data;
+        }
+    }
+    return "key not found";
+}
+
+
+void Tree::DeleteByKey(int key)
+{
+    if (key < 1)
+    {
+        std::cout << "incorrect key\n";
+        return;
+    }
+    if (!root)
+    {
+        std::cout << "tree is empty\n"; 
+        return;
+    }
+
+    Node *del, *prev_del, *r, *prev_r;
+    del = root;
+    prev_del = nullptr;
+
+    while (del && del->key != key)
+    {
+        prev_del = del;
+        if (del->key > key)
+            del = del->left;
+        else 
+            del = del->right;    
+    }
+
+    if (!del)
+    {
+        std::cout << "key not found\n";
+        return;
+    }
+
+    if (!del->right)
+        r = del->left;
+    else if (!del->left)
+        r = del->right;    
+    else 
+    {
+        prev_r = del;
+        r = del->left;
+        while (r->right)
+        {
+            prev_r = r;
+            r = r->right;
+        }
+
+        if (prev_r == del)
+            r->right = del->right;
+        else 
+        {
+            r->right = del->right;
+            prev_r->right = r->left;
+            r->left = prev_r;
+        }
+    }    
+
+    if (del == root)
+        root = r;
+    else if (del->key < prev_del->key)
+        prev_del->left = r;
+    else 
+        prev_del->right = r;
+    std::cout << "deleted info: " << del->key << ": " << del->data << std::endl;
+    delete del;        
+}
+
+
+int Tree::countLeaves() 
+{
+    Node * t = root;
+    return t->countLeaves(t);
 }
 
 
